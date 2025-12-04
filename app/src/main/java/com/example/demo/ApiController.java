aeac014ace9e4e8bfb4ce5d0e6c30a17d5a09bc9
NagendraOruganti <orugantinagendra2222@gmail.com>
2025-12-04 23:05:10 +0530

    Update ApiController.java

--- app/src/main/java/com/example/demo/ApiController.java
+++ app/src/main/java/com/example/demo/ApiController.java
@@ -1,1 +1,21 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @GetMapping("/")
+    public String root() {
+        System.out.println("Root endpoint called");
+        return "Hello from Java REST API behind ALB + ASG!";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        System.out.println("Health endpoint called");
+        return "ok";
+    }
+}
 
