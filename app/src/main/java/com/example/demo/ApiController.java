3ac0d8de1ac36024c1d4e1ab4dd67f578c7890b1
Manoj <manokamatam@gmail.com>
2025-12-05 01:57:34 +0530

    testing actions

--- app/src/main/java/com/example/demo/ApiController.java
+++ app/src/main/java/com/example/demo/ApiController.java
@@ -9,7 +9,7 @@ public class ApiController {
     @GetMapping("/")
     public String root() {
         System.out.println("Root endpoint called");
-        return "Hello from Java REST API behind ALB + ASG!";
+        return "Hello from Java REST API behind ALB + ASG! V1";
     }
 
     @GetMapping("/health")
