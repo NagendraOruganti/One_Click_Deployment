dca6840c913854e1218775baea5e47ff4abdcf32
NagendraOruganti <orugantinagendra2222@gmail.com>
2025-12-04 23:08:08 +0530

    Update destroy.sh

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -1,1 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+cd "$(dirname "$0")/../terraform"
+
+terraform destroy -auto-approve \
+  -var="app_s3_bucket=${APP_S3_BUCKET}" \
+  -var="app_s3_key=${APP_S3_KEY}" \
+  -var="aws_region=${AWS_REGION:-us-east-1}"
 
