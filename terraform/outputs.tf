10f18d32558f2a24eca8aba84777b9d2b368a593
NagendraOruganti <orugantinagendra2222@gmail.com>
2025-12-04 23:06:24 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,10 @@
+output "alb_dns_name" {
+  value       = aws_lb.app_alb.dns_name
+  description = "Public DNS name of the ALB"
+}
+
+output "api_url" {
+  value       = "http://${aws_lb.app_alb.dns_name}"
+  description = "Base URL of the API (HTTP)"
+}
 
